077198be0d033f8af923ac88c3b63e82ccd5a97b
Saiem Gilani <saiem.gilani@gmail.com>
2021-05-17 16:07:47 -0400

    scripts

--- R/team_page_calls.R
+++ R/team_page_calls.R
@@ -1,4 +1,3 @@
-library(kenpomR)
 library(tidyverse)
 library(rvest)
 library(xml2)
@@ -7,19 +6,20 @@ library(stringr)
 library(tidyr)
 library(stringr)
 library(xml2)
-library(git2r)
-source("pull_team_page.R")
+source("R/pull_team_page.R")
 teams = kenpomR::teams_links$Team
 yr=2021
-
 browser <- kenpomR::login(Sys.getenv("kp_user"),Sys.getenv("kp_pw"))
 gather_team_pages(browser, year = yr, fp="data")
-# New Year Data Repo Recreation
+
 repo <- git2r::repository('./') # Set up connection to repository folder
-cred <- git2r::cred_token()
 version <- "0.2.3"
 
 git2r::add(repo, glue::glue("data/*"))
 git2r::commit(repo, message = glue::glue("Updated Team Pages {Sys.time()} using kenpomR version {version}")) # commit the staged files with the chosen message
 git2r::pull(repo) # pull repo (and pray there are no merge commits)
-git2r::push(repo, credentials = git2r::cred_user_pass()) # push commit
+git2r::push(repo, credentials = git2r::cred_user_pass(username = Sys.getenv("GMAIL"), password = Sys.getenv("GH_PW"))) # push commit
+
+
+
+
--- R/update.R
+++ R/update.R
@@ -1,3 +1,4 @@
+library(kenpomR)
 library(tidyverse)
 library(rvest)
 library(xml2)
@@ -6,20 +7,19 @@ library(stringr)
 library(tidyr)
 library(stringr)
 library(xml2)
-source("pull_team_page.R")
+library(git2r)
+source("R/pull_team_page.R")
 teams = kenpomR::teams_links$Team
 yr=2021
-browser <- kenpomR::login(Sys.getenv("kp_user"),Sys.getenv("kp_pw"))
-gather_team_pages(browser, year = yr, fp="data")
 
+browser <- kenpomR::login(Sys.getenv("kp_user"),Sys.getenv("kp_pw"))
+gather_team_pages(year = yr, fp="data")
+# New Year Data Repo Recreation
 repo <- git2r::repository('./') # Set up connection to repository folder
+cred <- git2r::cred_token()
 version <- "0.2.3"
 
 git2r::add(repo, glue::glue("data/*"))
 git2r::commit(repo, message = glue::glue("Updated Team Pages {Sys.time()} using kenpomR version {version}")) # commit the staged files with the chosen message
 git2r::pull(repo) # pull repo (and pray there are no merge commits)
-git2r::push(repo, credentials = git2r::cred_user_pass(username = Sys.getenv("GMAIL"), password = Sys.getenv("GH_PW"))) # push commit
-
-
-
-
+git2r::push(repo, credentials = git2r::cred_user_pass()) # push commit
