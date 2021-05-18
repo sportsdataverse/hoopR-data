9cf0f5affd931ca8f9c6dea2cf67cde7332f6912
Saiem Gilani <saiem.gilani@gmail.com>
2021-05-18 07:34:22 -0400

    daily update

--- daily_nba_scraper.sh
+++ daily_nba_scraper.sh
@@ -2,7 +2,7 @@
 python scrape_nba_schedules.py
 python scrape_nba_json.py
 git add .
-git commit -m "daily update"
+git commit -m "daily update" || echo "No changes to commit"
 git push
 "C:\Program Files\R\R-4.0.5\bin\Rscript.exe" R/01_nba_pbp_creation.R
 git add .
