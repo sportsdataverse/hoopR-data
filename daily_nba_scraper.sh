6561f105db3d87b78fb4688419eeac0bb1b9ee9b
Saiem Gilani <saiem.gilani@gmail.com>
2021-05-18 07:10:29 -0400

    daily update

--- daily_nba_scraper.sh
+++ daily_nba_scraper.sh
@@ -5,5 +5,6 @@ git add .
 git commit -m "daily update"
 git pull --rebase && git push
 "C:\Program Files\R\R-4.0.5\bin\Rscript.exe" R/01_nba_pbp_creation.R
+git add .
 git commit -m "NBA Play-by-play and Schedules update" || echo "No changes to commit"
 git pull --rebase && git push
--- scrape_nba_json.py
+++ scrape_nba_json.py
@@ -23,52 +23,53 @@ def main():
     for year in reversed(years_arr):
         print(year)
         games = schedule[(schedule['season']==year)].reset_index()['game_id']
-        print(f"Number of Games: {len(games)}, first gameId: {games[0]}")
+        if len(games)>0:
+            print(f"Number of Games: {len(games)}, first gameId: {games[0]}")
 
-        # this finds our json files
-        path_to_raw_json = "{}/{}/".format(path_to_raw, year)
-        Path(path_to_raw_json).mkdir(parents=True, exist_ok=True)
-        # json_files = [pos_json.replace('.json', '') for pos_json in os.listdir(path_to_raw_json) if pos_json.endswith('.json')]
-        i = 0
-        for game in games[i:]:
-            if i == len(games):
-                print("done with year")
-                continue
-            if (i % 500 == 0 ):
-                print("Working on game {}/{}, gameId: {}".format(i+1, len(games[i:]) + i, game))
-            if len(str(game))<9:
+            # this finds our json files
+            path_to_raw_json = "{}/{}/".format(path_to_raw, year)
+            Path(path_to_raw_json).mkdir(parents=True, exist_ok=True)
+            # json_files = [pos_json.replace('.json', '') for pos_json in os.listdir(path_to_raw_json) if pos_json.endswith('.json')]
+            i = 0
+            for game in games[i:]:
+                if i == len(games):
+                    print("done with year")
+                    continue
+                if (i % 500 == 0 ):
+                    print("Working on game {}/{}, gameId: {}".format(i+1, len(games[i:]) + i, game))
+                if len(str(game))<9:
+                    i+=1
+                    continue
+                try:
+                    processor = PlayProcess( gameId = game, path_to_json = path_to_raw_json)
+                    np.warnings.filterwarnings('error', category=np.VisibleDeprecationWarning) 
+                    pbp = processor.nba_pbp()
+
+                except (TypeError) as e:
+                    print("TypeError: yo", e)
+                    pbp = processor.nba_pbp()
+
+                except (KeyError) as e:
+                    print("KeyError: yo", e)
+                    i+=1
+                    if i < len(games):
+                        print(f"Skipping game {i+1} of {len(games)}, gameId: {games[i]}")
+                    else:
+                        print(f"Skipping game {i} of {len(games)}")
+                    continue
+                except (ValueError) as e:
+                    print("DecodeError: yo", e)
+                    i+=1
+
+                    if i < len(games):
+                        print(f"Skipping game {i+1} of {len(games)}, gameId: {games[i]}")
+                    else:
+                        print(f"Skipping game {i} of {len(games)}")
+                    continue
+                fp = "{}{}.json".format(path_to_raw_json, game)
+                with open(fp,'w') as f:
+                    json.dump(pbp, f, indent=0, sort_keys=False)
                 i+=1
-                continue
-            try:
-                processor = PlayProcess( gameId = game, path_to_json = path_to_raw_json)
-                np.warnings.filterwarnings('error', category=np.VisibleDeprecationWarning) 
-                pbp = processor.nba_pbp()
-
-            except (TypeError) as e:
-                print("TypeError: yo", e)
-                pbp = processor.nba_pbp()
-
-            except (KeyError) as e:
-                print("KeyError: yo", e)
-                i+=1
-                if i < len(games):
-                    print(f"Skipping game {i+1} of {len(games)}, gameId: {games[i]}")
-                else:
-                    print(f"Skipping game {i} of {len(games)}")
-                continue
-            except (ValueError) as e:
-                print("DecodeError: yo", e)
-                i+=1
-
-                if i < len(games):
-                    print(f"Skipping game {i+1} of {len(games)}, gameId: {games[i]}")
-                else:
-                    print(f"Skipping game {i} of {len(games)}")
-                continue
-            fp = "{}{}.json".format(path_to_raw_json, game)
-            with open(fp,'w') as f:
-                json.dump(pbp, f, indent=0, sort_keys=False)
-            i+=1
         
 
 
