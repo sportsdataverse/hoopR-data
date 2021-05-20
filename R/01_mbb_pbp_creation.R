1e300a0ca864a2b85b690be0fd6b7a034bb07c57
Saiem Gilani <saiem.gilani@gmail.com>
2021-05-20 19:01:09 -0400

    daily update

--- R/01_mbb_pbp_creation.R
+++ R/01_mbb_pbp_creation.R
@@ -15,7 +15,7 @@ suppressPackageStartupMessages(suppressMessages(library(glue, lib.loc="C:/Users/
 
 options(stringsAsFactors = FALSE)
 options(scipen = 999)
-years_vec <- 2021:2021
+years_vec <- 2002:2021
 # --- compile into play_by_play_{year}.parquet ---------
 future::plan("multisession")
 progressr::with_progress({
