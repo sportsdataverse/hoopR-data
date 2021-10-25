4fb6f01846ef5516afa2b85ce79799777724da82
Saiem Gilani <saiem.gilani@gmail.com>
2021-10-25 05:18:55 -0400

    daily update

--- R/nba_stats_02_scrape_pbp.R
+++ R/nba_stats_02_scrape_pbp.R
@@ -1,7 +1,7 @@
 pacman::p_load("dplyr","purrr","stringr","data.table", "qs","arrow", "progressr")
 source('R/utils.R')
 
-years_vec <- 1997
+years_vec <- 1999
 schedules_df <-data.table::fread("nba_stats_schedule_master.csv")
 seasons_vec <- unlist(purrr::map(years_vec,function(x){year_to_season(x)})) 
 
@@ -19,7 +19,7 @@ year_json_scrape <- function(seasons_vec){
     nba_pbp_stats <- purrr::map_dfr(unique(schedules_year$GAME_ID), function(x){
       df <- hoopR::nba_pbp(game_id = x)
       jsonlite::write_json(df, path=paste0("nba_stats/",yr,"/",x,".json"))
-      Sys.sleep(2.5)
+      Sys.sleep(3)
     })
   }
 }
