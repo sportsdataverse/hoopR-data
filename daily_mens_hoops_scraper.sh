c2fefeed0eca207591ffb7707adb6b5df234630a
saiemgilani <saiem.gilani@gmail.com>
2022-04-22 03:08:30 -0400

    NBA and MBB Play-by-Play and Schedules update

--- daily_mens_hoops_scraper.sh
+++ daily_mens_hoops_scraper.sh
@@ -2,6 +2,6 @@
 bash daily_nba_scraper.sh
 bash daily_mbb_scraper.sh
 git pull
-git commit -m "NBA and MBB Play-by-play and Schedules update" || echo "No changes to commit"
+git commit -m "NBA and MBB Play-by-Play and Schedules update" || echo "No changes to commit"
 git pull
 git push
