4a143193b62f5a86130a8b5a770a209ebefa60aa
saiemgilani <saiem.gilani@gmail.com>
2022-05-29 09:30:16 -0400

    nba

--- R/nba_stats_draftcombinedrillresults.R
+++ R/nba_stats_draftcombinedrillresults.R
@@ -1,3 +1,6 @@
 combine_players <- hoopR::nba_draftcombinedrillresults(season = 2022)$Results
 
 readr::write_csv(combine_players,"nba/draft/nba_draftcombinedrillresults_2022.csv")
+
+
+jsonlite::write_json(combine_players,"nba/draft/json/nba_draftcombinedrillresults_2022.json")
