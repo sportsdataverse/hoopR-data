1af53aa753c663efa675990284fedf2399a898ed
saiemgilani <saiem.gilani@gmail.com>
2022-05-29 18:17:08 -0400

    draft

--- R/nba_stats_draftcombinedrillresults.R
+++ R/nba_stats_draftcombinedrillresults.R
@@ -11,8 +11,11 @@ combine_players <- combine_players_stats %>%
 # combine_players_history <- hoopR::nba_drafthistory(season=2021)$DraftHistory
 readr::write_csv(combine_players,"nba/draft/nba_draft_2022.csv")
 
-
-jsonlite::write_json(combine_players,"nba/draft/json/nba_draft_2022.json")
+combine_players <- combine_players_stats %>% 
+  dplyr::left_join(combine_players_spot,by=c("PLAYER_ID","FIRST_NAME","LAST_NAME","PLAYER_NAME","POSITION")) %>% 
+  dplyr::left_join(combine_players_shooting,by=c("PLAYER_ID","FIRST_NAME","LAST_NAME","PLAYER_NAME","POSITION")) %>% 
+  as.data.frame()
+jsonlite::write_json(jsonlite::toJSON(combine_players),"nba/draft/json/nba_draft_2022.json")
 jsonlite::write_json(combine_players_shooting,"nba/draft/json/nba_draftcombinenonstationaryshooting_2022.json")
 jsonlite::write_json(combine_players_spot,"nba/draft/json/nba_draftcombinespotshooting_2022.json")
 jsonlite::write_json(combine_players_stats,"nba/draft/json/nba_draftcombinestats_2022.json")
